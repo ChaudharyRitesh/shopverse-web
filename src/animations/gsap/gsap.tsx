7779e72c812f47c5cdd1e43d0030341de5b65852
ChaudharyRitesh <ritesh.chaudhari.t5a17@icp.edu.np>
2024-11-01 15:43:40 +0530

    jhjhvjh

---
+++ src/animations/gsap/gsap.tsx
@@ -0,0 +1,41 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+gsap.registerPlugin(useGSAP);
+
+const GsapAnimation = () => {
+  useGSAP(() => {
+    gsap.to("#box1", {
+      x: 1000,
+      duration: 1.5,
+      delay: 1,
+      stagger: 0.3,
+
+      repeatDelay: 0.8,
+    });
+    gsap.to("#box2", {
+      x: 1000,
+      duration: 1.5,
+      delay: 2.5,
+      stagger: 0.5,
+      repeatDelay: 0.8,
+    });
+    gsap.to("#box3", {
+      x: 1000,
+      duration: 1.5,
+      delay: 3,
+      stagger: 1,
+
+      repeatDelay: 0.8,
+    });
+  });
+  return (
+    <div className="container space-y-2">
+      <div id="box1" className="box w-[100px] h-[100px] bg-red-500"></div>
+      <div id="box2" className="box w-[100px] h-[100px] bg-blue-500"></div>
+      <div id="box3" className="box w-[100px] h-[100px] bg-green-500"></div>
+    </div>
+  );
+};
+
+export default GsapAnimation;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,34 +1,62 @@
-import { useState, useEffect } from "react";
-import useTheme from "../hooks/usetTheme";
-import "../App.css";
+// import { useState, useEffect } from "react";
+// import gsap from "gsap";
+// import useTheme from "../hooks/usetTheme";
+// import "../App.css";
+import GsapAnimation from "../animations/gsap/gsap";
 
 const HomePage = () => {
-  const { setTheme, theme } = useTheme();
-  const [isTransitioning, setIsTransitioning] = useState(false);
+  // const { setTheme, theme } = useTheme();
+  // const [isTransitioning, setIsTransitioning] = useState(false);
 
-  // Trigger the theme transition effect when the theme changes
-  useEffect(() => {
-    if (isTransitioning) {
-      const timeout = setTimeout(() => setIsTransitioning(false), 500); // Animation duration
-      return () => clearTimeout(timeout);
-    }
-  }, [isTransitioning]);
+  // useEffect(() => {
+  //   if (isTransitioning) {
+  //     const timeout = setTimeout(() => setIsTransitioning(false), 500);
+  //     return () => clearTimeout(timeout);
+  //   }
+  // }, [isTransitioning]);
 
-  const handleThemeChange = () => {
-    setIsTransitioning(true);
-    setTheme(theme === "light" ? "dark" : "light");
-    localStorage.setItem("theme", theme === "light" ? "dark" : "light");
-  };
+  // const handleThemeChange = () => {
+  //   setIsTransitioning(true);
+  //   setTheme(theme === "light" ? "dark" : "light");
+  //   localStorage.setItem("theme", theme === "light" ? "dark" : "light");
+  // };
+
+  // gsap.to("#box1", {
+  //   x: 1000,
+  //   duration: 2,
+  //   delay: 2,
+  //   rotate: 360,
+  // });
+
+  // gsap.from("#box", { rotation: -360, x: -100, duration: 1 });
+
+  // gsap.fromTo("#box", { x: -100 }, { rotation: 360, x: 100, duration: 1 });
+
+  // gsap.to("#box1", {
+  //   // selector text, Array, or object
+  //   x: 1000, // any properties (not limited to CSS)
+  //   backgroundColor: "red", // camelCase
+  //   duration: 1.5, // seconds
+  //   delay: 1,
+  //   ease: "power2.inOut",
+  //   scale: 0.4,
+  //   stagger: 0.3, // stagger start times
+  //   paused: false, // default is false
+  //   overwrite: "auto", // default is false
+  //   repeat: 2, // number of repeats (-1 for infinite)
+  //   repeatDelay: 0.5, // seconds between repeats
+  //   repeatRefresh: true, // invalidates on each repeat
+  //   yoyo: true, // if true > A-B-B-A, if false > A-B-A-B
+  //   yoyoEase: true, // or ease like "power2"
+  //   immediateRender: false,
+  //   onComplete: () => {
+  //     console.log("finished");
+  //   },
+  // });
 
   return (
-    <div className={`theme-container ${theme}`}>
-      <h1>Home Page</h1>
-      <p>Current Theme: {theme}</p>
-
-      <button onClick={handleThemeChange}>Change Theme</button>
-
-      {/* Theme transition overlay */}
-      {isTransitioning && <div className="theme-transition active" />}
+    <div>
+      <GsapAnimation />
     </div>
   );
 };
